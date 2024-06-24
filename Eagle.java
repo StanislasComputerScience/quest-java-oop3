7e2b0083e48fe2837dc7ef8b220996822a7d664d
StanislasComputerScience <remilabonne@yahoo.fr>
2024-06-24 02:15:49 +0200

    j'envoie ma solution

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,5 @@
-public class Eagle extends Bird {
+//Eagle.java
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +22,61 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    // décoller
+    @Override
+    public void takeOff() {
+        this.flying = true;
+        System.out.println("Hawkeye takes off in the sky.");
+    }
+
+    // monter
+    @Override
+    public void ascend(int altitude) {
+        this.altitude += altitude;
+        System.out.printf("Hawkeye flies upward, altitude: %d%n", this.altitude);
+
+    }
+
+    // descendre
+    @Override
+    public void descend(int altitude) {
+        this.altitude -= altitude;
+        System.out.printf("Hawkeye flies downward, altitude: %d%n", this.altitude);
+    }
+
+    // @Override
+    // public void descend(int altitude) {
+    // this.altitude -= altitude;
+    // System.out.println("Hawkeye flies downward, altitude: %d", );
+    // }
+
+    // atterrir
+    @Override
+    public void land() {
+        if (!isFlying()) {
+            System.out.println("Hawkeye has been still on the ground.");
+        } else if (this.altitude <= 1) {
+            System.out.println("Hawkeye lands on the ground.");
+            this.altitude = 0;
+        } else {
+            System.out.printf("Hawkeye is too high, it can't land, altitude : %d%n", this.altitude);
+        }
+    }
+
+    // @Override
+    // public void land() {
+    // if (altitude > 1) {
+    // System.out.println("Hawkeye is too high, it can't land.");
+    // } else {
+    // this.flying = false;
+    // System.out.println("Hawkeye lands on the ground.");
+    // }
+    // }
+
+    @Override
+    public void glide() {
+        System.out.println("glides into the air.");
+    }
+
 }
---
+++ Fly.java
@@ -0,0 +1,18 @@
+public interface Fly {
+    // décoller
+    void takeOff();
+
+    // monter
+    void ascend(int altitude);
+
+
+
+    // descendre
+    void descend(int altitude);
+
+    // atterrir
+    void land();
+
+    // planer
+    void glide();
+}
